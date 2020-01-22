38f36f61541246509dc6f252a13665c52f0ac886
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-01-22 18:24:21 +0100

    project tweeter implementation added

---
+++ Tweeters/src/main/java/pl/tweeter/Tweeter/TweeterApplication.java
@@ -0,0 +1,13 @@
+package pl.tweeter.Tweeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterApplication.class, args);
+	}
+
+}
---
+++ Tweeters/src/test/java/pl/tweeter/Tweeter/TweeterApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.tweeter.Tweeter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TweeterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
