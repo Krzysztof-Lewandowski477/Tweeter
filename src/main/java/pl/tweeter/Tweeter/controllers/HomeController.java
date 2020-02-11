78d26fccdb4243954699c5eefabb1f6f8f8af09f
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-02-11 17:37:36 +0100

    add form adding twitts by login user and view this form in page

--- src/main/java/pl/tweeter/Tweeter/controllers/HomeController.java
+++ src/main/java/pl/tweeter/Tweeter/controllers/HomeController.java
@@ -3,11 +3,16 @@ package pl.tweeter.Tweeter.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.tweeter.Tweeter.domain.entity.Tweet;
-import pl.tweeter.Tweeter.domain.entity.User;
 import pl.tweeter.Tweeter.domain.repositories.TweetRepository;
+import pl.tweeter.Tweeter.dtos.UserTwittDataDTO;
+import pl.tweeter.Tweeter.services.impl.UserTwittServiceImpl;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -15,9 +20,11 @@ import java.util.List;
 public class HomeController {
 
     private final TweetRepository tweetRepository;
+    private final UserTwittServiceImpl userTwittService;
 
-    public HomeController(TweetRepository tweetRepository) {
+    public HomeController(TweetRepository tweetRepository, UserTwittServiceImpl userTwittService) {
         this.tweetRepository = tweetRepository;
+        this.userTwittService = userTwittService;
     }
 
     @GetMapping("/")
@@ -27,10 +34,23 @@ public class HomeController {
     }
 
     @GetMapping("/index")
-    public String getIndexPage(Model model){
+    public String getIndexPage(Model model ){
         List<Tweet> tweets = tweetRepository.findAll ();
         model.addAttribute ( "tweets" , tweets );
+        model.addAttribute ( "newTweet", new UserTwittDataDTO ());
+
         return "index";
     }
+
+    @PostMapping("/index")
+    public String postIndexPage(@ModelAttribute("newTweet") @Valid UserTwittDataDTO userTwittDataDTO , BindingResult result){
+        if(result.hasErrors ()){
+            return "redirect:/index";
+        }
+        userTwittService.addTweet ( userTwittDataDTO );
+        return "redirect:/index";
+    }
+
+
 
 }
