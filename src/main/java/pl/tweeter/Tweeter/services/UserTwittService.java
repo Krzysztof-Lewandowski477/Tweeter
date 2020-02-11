899aad368ef3460dd1e3add9ecf06b22afc2aad7
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-02-11 17:38:10 +0100

    add metohod addTweet by users

--- src/main/java/pl/tweeter/Tweeter/services/UserTwittService.java
+++ src/main/java/pl/tweeter/Tweeter/services/UserTwittService.java
@@ -1,8 +1,13 @@
 package pl.tweeter.Tweeter.services;
 
+
 import pl.tweeter.Tweeter.dtos.UserTwittDataDTO;
 
 public interface UserTwittService {
 
     void showTwitt(Long id);
+
+    void addTweet(UserTwittDataDTO userTwittDataDTO);
+
+
 }
--- src/main/java/pl/tweeter/Tweeter/services/impl/UserTwittServiceImpl.java
+++ src/main/java/pl/tweeter/Tweeter/services/impl/UserTwittServiceImpl.java
@@ -2,11 +2,17 @@ package pl.tweeter.Tweeter.services.impl;
 
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.tweeter.Tweeter.domain.entity.Tweet;
 import pl.tweeter.Tweeter.domain.repositories.TweetRepository;
 import pl.tweeter.Tweeter.domain.repositories.UserRepository;
 
+import pl.tweeter.Tweeter.dtos.UserTwittDataDTO;
 import pl.tweeter.Tweeter.services.UserTwittService;
-
+@Service
+@Transactional
 public class UserTwittServiceImpl implements UserTwittService {
 
     private final UserRepository userRepository;
@@ -21,5 +27,14 @@ public class UserTwittServiceImpl implements UserTwittService {
     public void showTwitt(Long id) {
 
         tweetRepository.findAllById ( id );
+    }
+
+    @Override
+    public void addTweet(UserTwittDataDTO userTwittDataDTO) {
+
+        ModelMapper modelMapper = new ModelMapper ();
+        Tweet tweet = modelMapper.map ( userTwittDataDTO,Tweet.class );
+        tweetRepository.save ( tweet );
+
     }
 }
