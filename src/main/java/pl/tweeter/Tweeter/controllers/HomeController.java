6d06b44726d50191ad127a99dc0faef57ee31894
Krzysztof Lewandowski <krzysztoflewandowski477@gmail.com>
2020-02-11 14:24:37 +0100

    HomeController add twitts import to jsp

--- src/main/java/pl/tweeter/Tweeter/controllers/HomeController.java
+++ src/main/java/pl/tweeter/Tweeter/controllers/HomeController.java
@@ -2,10 +2,23 @@ package pl.tweeter.Tweeter.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.tweeter.Tweeter.domain.entity.Tweet;
+import pl.tweeter.Tweeter.domain.entity.User;
+import pl.tweeter.Tweeter.domain.repositories.TweetRepository;
+
+import java.util.List;
+
 @Slf4j
 @Controller
 public class HomeController {
+
+    private final TweetRepository tweetRepository;
+
+    public HomeController(TweetRepository tweetRepository) {
+        this.tweetRepository = tweetRepository;
+    }
 
     @GetMapping("/")
     public String  getHomePage(){
@@ -14,8 +27,9 @@ public class HomeController {
     }
 
     @GetMapping("/index")
-    public String getIndexPage(){
-
+    public String getIndexPage(Model model){
+        List<Tweet> tweets = tweetRepository.findAll ();
+        model.addAttribute ( "tweets" , tweets );
         return "index";
     }
 
